0b8e7dd46958116f4f081b302f024607f1d64846
Andrew Weiner <andrew.weiner@phenixrts.com>
2022-01-25 09:23:43 -0700

    * Input and output directories are now passed by command line arg. * Better error handing. e.g. Exit with error message if date regex fails. * More debug output * Handling different table column names in July 2020

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import tabula
 import PyPDF2
 import re
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 from datetime import datetime
 import math
 import pandas
+import sys
 
-path_to_report_directory = "/Users/andrew/projects/covid-19-pediatric/pediatric-state-reports-to-process/"
-path_to_output_directory = "/Users/andrew/projects/covid-19-pediatric/scraped_data/"
+# path_to_report_directory = "/Users/andrew/projects/covid-19-pediatric/pediatric-state-reports-to-process/"
+path_to_report_directory = sys.argv[1]
+# path_to_output_directory = "/Users/andrew/projects/covid-19-pediatric/scraped_data/"
+path_to_output_directory = sys.argv[2]
 
 
 def save_tables_to_csv(tables, output_file_path, column_headers):
@@ -58,9 +61,23 @@ def save_tables_to_csv(tables, output_file_path, column_headers):
 
 
 if __name__ == '__main__':
+    if not isdir(path_to_report_directory):
+        print(f'ERROR: [{path_to_report_directory}] is not a valid directory')
+        exit(1)
+
+    if not isdir(path_to_output_directory):
+        print(f'ERROR: [{path_to_output_directory}] is not a valid directory')
+        exit(1)
+
+    print(f'Processing files from report directory [{path_to_report_directory}]')
+    print(f'Using output directory [{path_to_output_directory}]')
     filenames = [f for f in listdir(path_to_report_directory) if isfile(join(path_to_report_directory, f))]
 
     for filename in filenames:
+        if not filename.lower().endswith('.pdf'):
+            print(f'Skipping file [{filename}] because not a pdf.')
+            continue
+
         print(f'Processing file [{filename}]')
         file = join(path_to_report_directory, filename)
         pdfFileObj = open(file, 'rb')
@@ -74,8 +91,14 @@ if __name__ == '__main__':
         # extracting text from page
         page1_text = pageObj.extractText()
 
-        regex = "Version: ([0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4})"
-        date_string = re.search(regex, page1_text).group(1)
+        regex = "Version\n{0,1}: ([0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4})"
+        regex_match = re.search(regex, page1_text)
+
+        if regex_match is None:
+            print(f'Unable to find regex [{regex}] in text [{page1_text}]')
+            exit(1)
+
+        date_string = regex_match.group(1)
         date = datetime.strptime(date_string, '%m/%d/%y')
         print(date)
 
@@ -91,16 +114,30 @@ if __name__ == '__main__':
             print(table.columns)
             print(row_0)
 
-            if ("Cumulative total cases" in table.columns and len(table.columns) == 8 and (
-                    "Location" in table.columns or "Location" in row_0)):
+            if (("Cumulative total cases" in table.columns or "Number of child cases" in row_0)
+                    and len(table.columns) == 8
+                    and ("Location" in table.columns or "Location" in row_0)):
                 case_tables.append(table)
                 print(f"Found case table:\n{table}")
+            else:
+                print(f'Not the case table...')
+                print(f'* "Cumulative total cases" in table.columns: {"Cumulative total cases" in table.columns}')
+                print(f'* len(table.columns) == 8: {len(table.columns) == 8}')
+                print(f'* "Location" in table.columns: {"Location" in table.columns}')
+                print(f'* "Location" in row_0: {"Location" in row_0}')
 
             if ("Percent children of total" in table.columns and (
                     len(table.columns) == 6 or len(table.columns) == 7) and (
                     "Location" in table.columns or "Location" in row_0)):
                 hospitalization_tables.append(table)
                 print(f"Found hospitalization table:\n{table}")
+            else:
+                print(f'Not the hospitalization table...')
+                print(f'* "Percent children of total" in table.columns: {"Percent children of total" in table.columns}')
+                print(f'* len(table.columns) == 6: {len(table.columns) == 6}')
+                print(f'* len(table.columns) == 7: {len(table.columns) == 7}')
+                print(f'* "Location" in table.columns: {"Location" in table.columns}')
+                print(f'* "Location" in row_0: {"Location" in row_0}')
 
         output_filename = f"cases-by-state-{date.strftime('%Y-%m-%d')}.csv"
         output_file_path = join(path_to_output_directory, output_filename)
@@ -108,6 +145,11 @@ if __name__ == '__main__':
                           "Cumulative child cases",
                           "Percent children of total cases", "Cumulative total cases (all ages)",
                           "Cases per 100,000 children"]
+
+        if len(case_tables) == 0:
+            print(f'ERROR: No case tables found')
+            exit(1)
+
         save_tables_to_csv(case_tables, output_file_path, column_headers)
 
         output_filename = f"hospitalizations-by-state-{date.strftime('%Y-%m-%d')}.csv"
