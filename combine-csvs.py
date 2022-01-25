207a1c18d90cdc2ec0463ec7389c14ac930ad204
Andrew Weiner <andrew.weiner@phenixrts.com>
2022-01-25 09:22:59 -0700

    Input and output directories are now passed via command line arg

--- combine-csvs.py
+++ combine-csvs.py
@@ -1,9 +1,12 @@
 import pandas
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
+import sys
 
-path_to_input_directory = "/Users/andrew/projects/covid-19-pediatric/scraped_data/"
-path_to_output_directory = "/Users/andrew/projects/covid-19-pediatric/combined_data/"
+# path_to_input_directory = "/Users/andrew/projects/covid-19-pediatric/scraped_data/"
+path_to_input_directory = sys.argv[1]
+# path_to_output_directory = "/Users/andrew/projects/covid-19-pediatric/combined_data/"
+path_to_output_directory = sys.argv[2]
 
 
 def combine_csvs(input_csvs, output_filename):
@@ -22,6 +25,14 @@ def combine_csvs(input_csvs, output_filename):
 
 
 if __name__ == '__main__':
+    if not isdir(path_to_input_directory):
+        print(f'ERROR: [{path_to_input_directory}] is not a valid directory')
+        exit(1)
+
+    if not isdir(path_to_output_directory):
+        print(f'ERROR: [{path_to_output_directory}] is not a valid directory')
+        exit(1)
+
     cases_filenames = [f for f in listdir(path_to_input_directory) if
                        isfile(join(path_to_input_directory, f)) and "cases" in f]
     hospitalizations_filenames = [f for f in listdir(path_to_input_directory) if
